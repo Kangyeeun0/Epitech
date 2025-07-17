8ace055a85da97cda1e726306fd4f43a710f601a
余凌歌 <yulingge@yulinggedeMacBook-Pro.local>
2025-07-17 10:27:49 +0200

    feat:info

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+// import axios from 'axios';
 import { useState } from 'react';
 import './register.css';
 
